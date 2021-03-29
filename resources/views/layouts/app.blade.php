b2e89ac1f79330a97af2de149a041c7ea960fb32
mohamedsayedhameed <mohamedsayed71993@gmail.com>
2021-03-30 00:28:34 +0200

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Photoshow</title>
+
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
+        @include('inc.navbar')
+        <br>
+        @include('inc.messages')
+        <br>
+        @yield('content')
+    </body>
+</html>
